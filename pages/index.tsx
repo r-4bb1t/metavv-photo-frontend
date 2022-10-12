e57f9578192e5d9fd2f2c6f124a1f8852bea618f
r4bb1t <r4bb1t.github@gmail.com>
2022-10-13 07:20:42 +0900

    add link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 import { Layout } from "../components/Layout";
 import styles from "../styles/Main.module.scss";
 
 const Footer = () => {
-  return <button className={styles.button}>만들기</button>;
+  return (
+    <Link href="/new">
+      <a className={styles.button}>만들기</a>
+    </Link>
+  );
 };
 
 const Home: NextPage = () => {
