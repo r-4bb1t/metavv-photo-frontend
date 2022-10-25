9e2c942d8f0976550fd90de4ea8dbfd96c5fd9ce
r4bb1t <r4bb1t.github@gmail.com>
2022-10-25 16:17:14 +0900

    fix many thing

--- pages/game/[gameId]/vote.tsx
+++ pages/game/[gameId]/vote.tsx
@@ -26,44 +26,70 @@ const Footer = () => {
 };
 const Home: NextPage = () => {
   const [modal, setModal] = useState(false);
-  const images = ["/assets/gamePage/dog2.jpg", "/assets/gamePage/dog3.jpg"];
-  const [image, setImage] = useState(images[0]);
+  const [images, setImages] = useState([] as { id: number; img: string }[]);
+  const [image, setImage] = useState("");
+  const router = useRouter();
+
+  const fetchData = useCallback(async () => {
+    try {
+      const result = await (
+        await fetch(
+          `${process.env.NEXT_PUBLIC_API_HOST}/${router.query.gameId}`
+        )
+      ).json();
+
+      setImages(result.photos);
+    } catch (e) {
+      console.log(e);
+    }
+  }, [router]);
+
+  useEffect(() => {
+    if (router.query.gameId) fetchData();
+  }, [fetchData, router]);
 
   return (
     <Layout footer={<Footer></Footer>} noBackground>
       <div className={styles.contents}>
-        <div className={styles.img1Area}>
-          <div className={styles.imgBorder1}>
-            <img className={styles.img1} src={images[0]}></img>
-          </div>
-          <img
-            className={styles.mag1}
-            onClick={() => {
-              setModal(!modal);
-              setImage(images[0]);
-            }}
-            src="/assets/gamePage/확대.svg"
-          ></img>
-        </div>
+        {images.length > 0 && (
+          <>
+            <div className={styles.img1Area}>
+              <div className={styles.imgBorder1}>
+                <img className={styles.img1} src={images[0].img} />
+              </div>
+              <img
+                className={styles.mag1}
+                onClick={() => {
+                  setModal((modal) => !modal);
+                  setImage(images[0].img);
+                }}
+                src="/assets/gamePage/확대.svg"
+              />
+            </div>
 
-        <div className={styles.img2Area}>
-          <div className={styles.imgBorder2}>
-            <img className={styles.img2} src={images[1]}></img>
-          </div>
-          <img
-            className={styles.mag2}
-            onClick={() => {
-              setModal(!modal);
-              setImage(images[1]);
-            }}
-            src="/assets/gamePage/확대.svg"
-          ></img>
-        </div>
-        <img className={styles.vs} src="/assets/gamePage/vs.png"></img>
+            <div className={styles.img2Area}>
+              <div className={styles.imgBorder2}>
+                <img className={styles.img2} src={images[1].img} />
+              </div>
+              <img
+                className={styles.mag2}
+                onClick={() => {
+                  setModal((modal) => !modal);
+                  setImage(images[1].img);
+                }}
+                src="/assets/gamePage/확대.svg"
+              />
+            </div>
+          </>
+        )}
+        <img className={styles.vs} src="/assets/gamePage/vs.png" />
 
-        {modal == true ? (
-          <Modal closeModal={() => setModal(!modal)} image={image}></Modal>
-        ) : null}
+        {modal && (
+          <Modal
+            closeModal={() => setModal((modal) => !modal)}
+            image={image}
+          ></Modal>
+        )}
       </div>
     </Layout>
   );
